29de891ff81939a285df9cc264bbae5ab9044f0d
Kiran Pillai <kirangopinath.2008@gmail.com>
2022-08-15 23:15:12 +0530

    commit pipe test cases

--- src/app/Pipe/transalation.pipe.spec.ts
+++ src/app/Pipe/transalation.pipe.spec.ts
@@ -5,4 +5,16 @@ describe('TransalationPipe', () => {
     const pipe = new TransalationPipe();
     expect(pipe).toBeTruthy();
   });
+  it('should return corresponding translation if translation key found', () => {
+    const pipe = new TransalationPipe();
+    const expected = 'Enter First Name';
+    let value = "FIRSTNAME_PLACEHOLDER";
+    expect(pipe.transform(value)).toEqual(expected);
+  });
+  it('should return empty string if translation key not found', () => {
+    const pipe = new TransalationPipe();
+    const expected = '';
+    let value = "FIRSTNAMESS_PLACEHOLDER";
+    expect(pipe.transform(value)).toEqual(expected);
+  });
 });
